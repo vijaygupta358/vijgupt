2d0457a8bffaeffebcb8b11bf64d50e75f9796a2
vijaygupta358 <vijay.gupta358@gmail.com>
2014-11-02 14:45:17 +0530

    commit1

--- aaa/src/com/logi/controllers/a.java
+++ aaa/src/com/logi/controllers/a.java
@@ -1,5 +1,11 @@
 package com.logi.controllers;
 
 public class a {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("vvh");
+System.out.print("vvh");
+System.out.print("vvh");
+	}
 
 }
